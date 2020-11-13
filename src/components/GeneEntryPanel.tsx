501826f818da85239e45fc5c80233a1d471f407a
D Otasek <dotasek.dev@gmail.com>
2020-11-12 19:29:21 -0500

    Make gene entry fetch task ID and forward to result page

--- src/components/GeneEntryPanel.tsx
+++ src/components/GeneEntryPanel.tsx
@@ -18,38 +18,54 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 )
 
-const GeneEntryPanel = (props : any) => {
-    const {genes, buttonText='Re-Run DrugCell'} = props;
+const GeneEntryPanel = (props: any) => {
+  const { genes, buttonText = 'Re-Run DrugCell' } = props;
 
-    const [geneInput, setGeneInput] = useState<string | undefined>(genes);
+  const [geneInput, setGeneInput] = useState<string | undefined>(genes);
 
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const history = useHistory();
+  const history = useHistory();
 
-    const handleUpdate = (event : any) => {
-        setGeneInput(event.target.value);
-    }
+  const handleUpdate = (event: any) => {
+    setGeneInput(event.target.value);
+  }
 
-    const handleClick = () => {
-      history.push("/finddrugs/results/" + geneInput);
-    }
+  const handleClick = () => {
+    console.log('sending gene input: ', geneInput);
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        "algorithm": "drugcellfinddrug",
+        "data": geneInput
+      })
+    };
+    fetch('http://localhost:8081/cd/communitydetection/v1', requestOptions)
+      .then(response => response.json())
+      .then(data => {
+        console.log('response', data)
+        history.push("/finddrugs/results/" + data.id)
+      }
+      );
 
-    return ( 
+  }
+
+  return (
     <div className={classes.container}>
-    <TextField
+      <TextField
         id='standard-multiline-static'
         label="Gene List"
         multiline
         rows={4}
         value={geneInput}
-        onChange={handleUpdate}    
-    />
-       <Button variant="contained" color="primary" onClick={handleClick}>
-            { buttonText }
-        </Button>
+        onChange={handleUpdate}
+      />
+      <Button variant="contained" color="primary" onClick={handleClick}>
+        {buttonText}
+      </Button>
     </div>
-    )
+  )
 }
-  
+
 export default GeneEntryPanel;
