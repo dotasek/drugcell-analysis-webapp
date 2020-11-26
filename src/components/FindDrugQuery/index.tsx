10471a639d401bfcafbddd4148b542ed18641da5
D Otasek <dotasek.dev@gmail.com>
2020-11-26 12:29:46 -0500

    Add slide effect for hint

--- src/components/FindDrugQuery/index.tsx
+++ src/components/FindDrugQuery/index.tsx
@@ -3,6 +3,7 @@ import GeneEntryPanel from '../GeneEntryPanel'
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import { Typography } from '@material-ui/core';
 
+import Slide from '@material-ui/core/Slide'; 
 
 import './style.css';
 
@@ -14,10 +15,12 @@ const FindDrugQuery = (props: any) => {
         <GeneEntryPanel genes={""} {...props} />
       </div>
       <div className='center-components'>
+      <Slide direction="left" in={true} mountOnEnter unmountOnExit timeout={1500} > 
         <div className='hint'>
           <ArrowBackIcon></ArrowBackIcon>
           <Typography>Enter Query Genes and then Click 'RUN DRUGCELL' to begin analysis.</Typography>
         </div>
+        </Slide>
       </div>
     </div>
   );
